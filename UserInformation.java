fc5dff6d3805db1bb1d38d93aad556056d6def61
Shreyansh Goyal <shreyanshg2010@gmail.com>
2018-06-08 15:45:01 +0530

    Create UserInformation.java

---
+++ UserInformation.java
@@ -0,0 +1,47 @@
+package co.shrey.fireapp;
+
+import java.io.Serializable;
+
+/**
+ * Created by shrey on 01-06-2018.
+ */
+
+public class UserInformation implements Serializable {
+    private String name;
+    private String address;
+    private String age;
+
+    public UserInformation() {
+
+    }
+
+    public UserInformation(String name, String address, String age) {
+        this.name = name;
+        this.address = address;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
